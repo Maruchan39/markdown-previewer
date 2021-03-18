39dace3389cf5d52c160b70d94b6f07766cb008a
Martynas Baltutis <martynas.baltutis@gmail.com>
2021-03-18 22:27:12 +0200

    set up parser

--- src/Preview.js
+++ src/Preview.js
@@ -1,8 +1,14 @@
-import React from 'react';
-const Previewer = ({text}) => {
-    return <div>
-        <p>Text here:{text}</p>
-    </div>
-}
+import React, { useState, useEffect } from "react";
+import marked from "marked";
+// Import actually seems to be sucessfull
+
+const Previewer = ({ text }) => {
+  const getMarkdownText = (text) => {
+    let rawMarkup = marked(text);
+    return { __html: rawMarkup };
+  };
+
+  return <div dangerouslySetInnerHTML={getMarkdownText(text)} />;
+};
 
 export default Previewer;
